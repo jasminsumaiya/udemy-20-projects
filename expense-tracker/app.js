58aee1c7a456a5c7b9a7a57454cd20fd3185257a
jasminsumaiya <sumaiya@provility.com>
2020-12-18 16:23:52 +0530

    completed expence tracker

--- expense-tracker/app.js
+++ expense-tracker/app.js
@@ -47,9 +47,9 @@ function showSuccess(domElement) {
 
 function refreshDomElement() {
     renderHistry();
-    //renderIncome();
-    //renderExpence();
-    //renderBalance();
+    renderIncome();
+    renderExpence();
+    renderBalance();
 }
 
 function renderHistry() {
@@ -59,8 +59,46 @@ function renderHistry() {
                         return `<li class="histry-list" data-status="${status}">
                                     <span class="col1">${transaction.content}</span>
                                     <span class="col2">${transaction.amount}</span>
-                                    <button class="delite-btn col3">X</button>
+                                    <button class="delite-btn col3" onclick="deliteItem(event)" data-delite-id="${transaction.ID}">X</button>
                                 </li>`
     }).join(" ");
+}
+
+function renderIncome() {
+    let income = document.getElementById("money-plus");
+    let incomeAmount = 0;
+    transactionList.forEach((transactionItem) => {
+        if(transactionItem.amount > 0){
+            incomeAmount =  parseInt(incomeAmount) + parseInt(transactionItem.amount);;
+        }
+    });
+    income.textContent = `+$ ${incomeAmount}`;
+}
+
+function renderExpence() {
+    let expense = document.getElementById("money-minus");
+    let expenseAmount = 0;
+    transactionList.forEach((transactionItem) => {
+        if(transactionItem.amount < 0){
+            expenseAmount =  parseInt(expenseAmount) + parseInt(transactionItem.amount);
+        }
+    });
+    expense.textContent = `-$ ${Math.abs(expenseAmount)}`;
+}
+
+function renderBalance() {
+    let balance = document.getElementById("balance");
+    let balanceAmound = 0;
+    transactionList.forEach((transactionItem) => {
+        balanceAmound += parseInt(transactionItem.amount);
+    });
+    balance.textContent = `$ ${balanceAmound}`;
+}
+
+function deliteItem(e) {
+    let currentID = e.currentTarget.getAttribute("data-delite-id");
+    let filteredList = transactionList.filter((transaction) => transaction.ID != currentID);
+    transactionList = filteredList;
+    refreshDomElement();
 }
 
