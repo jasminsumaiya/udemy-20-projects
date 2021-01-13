c94e01b4b24909d86eb3a2627aa8d6fa0294a1d8
Jasmin Sumaiya <sumaiya@macs-MacBook-Pro.local>
2021-01-13 09:59:30 +0530

    implemented music-player project

--- music-player/app.js
+++ music-player/app.js
@@ -2,20 +2,30 @@
 window.addEventListener('load', init);
 let onPlay = false;
 
+let musicLabelList = ["1.hey", "2.summer", "3.ukulele"];
+let musics = ["assets/musics/hey.mp3", "assets/musics/summer.mp3", "assets/musics/ukulele.mp3"];
+let images = ["assets/images/hey.jpg", "assets/images/summer.jpg", "assets/images/ukulele.jpg"];
+let activeMusicIndex = 0;
+
 function init() {
     let audio = document.getElementById("my-audio");
     audio.addEventListener("timeupdate", updateProgress);
 
     let backward = document.getElementById("backward");
+    backward.addEventListener("click", onClickBackward);
+
     let forward = document.getElementById("forward");
+    forward.addEventListener("click", onClickForward);
 
     let play = document.getElementById("play");
-    play.addEventListener('click', () => {
+    play.addEventListener('click', onPlayClick);
+}
+
+function onPlayClick() {
         toggleAudioStatus();
         updatePlayIcon();
         showProgressBar();
         rotateImage();
-    });
 }
 
 //audio play and pause
@@ -25,6 +35,8 @@ function toggleAudioStatus() {
     if (onPlay == false) {
         audio.play();
         onPlay = true;
+        let musicLabelDom = document.getElementsByTagName("p")[0];
+        musicLabelDom.textContent = musicLabelList[activeMusicIndex];
     } else {
         audio.pause();
         onPlay = false;
@@ -55,7 +67,7 @@ function showProgressBar() {
 
 // rotate image
 function rotateImage() {
-    let imageDiv = document.getElementById("image");
+    let imageDiv = document.getElementById("music-image");
 
     if (onPlay) {
         imageDiv.classList.add("img-rotate");
@@ -70,4 +82,43 @@ function updateProgress() {
     const audio = document.getElementById("my-audio");
 
     progress.value = (audio.currentTime / audio.duration) * 100;
+}
+
+//on click Backward
+function onClickBackward() {
+    activeMusicIndex --;
+    if(activeMusicIndex < 0){
+        activeMusicIndex = musics.length - 1;
+    } 
+    
+    updatePlayList();
+}
+
+//on click Forward
+function onClickForward() {
+    activeMusicIndex ++; 
+    if(activeMusicIndex > musics.length - 1){
+        activeMusicIndex = 0
+    } 
+    
+    updatePlayList();
+}
+
+// update music and image
+function updatePlayList() {
+    const audio = document.getElementById("my-audio");
+    if (onPlay) {
+        audio.currentTime = 0;
+        audio.pause();
+    }
+
+    const audioSourceDom = document.getElementById("mp3-source");
+    audioSourceDom.src = musics[activeMusicIndex];
+    const imageDom = document.getElementById("music-image");
+    imageDom.src = images[activeMusicIndex];
+
+    onPlay = false;
+    showProgressBar();
+    audio.load();
+    onPlayClick();
 }
