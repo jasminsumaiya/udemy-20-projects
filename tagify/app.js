da6f23b53d7288c64d74c9b0ec9def54b5dbb812
jasminsumaiya <sumaiya@provility.com>
2021-01-18 09:00:52 +0530

    completed tagify project

--- tagify/app.js
+++ tagify/app.js
@@ -3,63 +3,58 @@ let countryNameList = ["Afghanistan", "Algeria", "Argentina", "Austria", "Bahrai
     "Belgium", "Bolivia", "Brazil", "Cabo-Verde", "Cambodia", "Cameroon", "Canada",
     "China", "congo", "Denmark", "Dominica", "India", "Norway", "Pakistan"];
 
-let filteredListItem = [];
 let tagList = [];
-
 
 function onInputChange(e) {
     let input = document.getElementById("text-box").value.toLowerCase();
     let dropdown = document.getElementById("dropdown");
-
     if (input == "") {
         dropdown.innerHTML = "";
         return;
     }
 
-    filteredListItem = countryNameList.filter((countryName, index) => {
+    let filteredListItem = countryNameList.filter((countryName) => {
         return countryName.toLowerCase().includes(input);
     });
-
-    renderListTags();
+    renderListTags(filteredListItem);
 }
 
-function renderListTags() {
-    let listItem = filteredListItem.map((item) => `<li class="country-list" data-country-name="${item}" onClick="onclickTag(event)">
+function renderListTags(filteredListItem) {
+    let dropdown = document.getElementById("dropdown");
+    let listItem = filteredListItem.map((item) => `<li class="country-list" data-country-name="${item}" onClick="onSelectCountry(event)">
                                                         <img src="country_and_flags/${item}.png" id="country-flag">
                                                         <span class="country-name">${item}</span>
                                                     </li>`).join(" ");
     dropdown.innerHTML = `<ul class="dropdown_menu-6">${listItem}</ul>`;
 }
 
-function onclickTag(e) {
+function onSelectCountry(e) {
+    let selectedCountryName = e.currentTarget.getAttribute("data-country-name");
+    tagList.push(selectedCountryName);
+    renderTags();
+
     let input = document.getElementById("text-box");
     input.value = "";
-    let target = e.currentTarget.getAttribute("data-country-name");
-    tagList.push(target);
-    countryNameList = countryNameList.filter((countryName) => countryName != target);
-
+    countryNameList = countryNameList.filter((countryName) => countryName != selectedCountryName);
     onInputChange();
-    console.log(countryNameList);
-    renderTags();
 }
 
 function renderTags() {
     let tagItems = document.getElementById("tag-items");
-
     tagItems.innerHTML = tagList.map((item) => {
         return `<span id="tag" class="col-sm tag alignleft">
                                     <img src="country_and_flags/${item}.png" id="flag_img">
                                     <p>${item}</p>
-                                    <i class="fa fa-times" id="trace" aria-hidden="true" onClick=onDeleteClick(event) data-delete-name="${item}"></i>
+                                    <i class="fa fa-times" id="trace" aria-hidden="true" onClick=onRemoveCountry(event) data-country-name="${item}"></i>
                              </span>`}).join(" ");
     console.log(tagList);
 }
 
-function onDeleteClick(e) {
-    let target = e.currentTarget.getAttribute("data-delete-name");
-    tagList = tagList.filter((item) => item != target);
+function onRemoveCountry(e) {
+    let selectedCountryName = e.currentTarget.getAttribute("data-country-name");
+    tagList = tagList.filter((item) => item != selectedCountryName);
     renderTags();
 
-    countryNameList.unshift(target);
+    countryNameList.unshift(selectedCountryName);
     onInputChange();
 }
