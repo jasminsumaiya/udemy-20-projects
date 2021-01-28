ef06aa5a095a7283e806204391623c94ca96d649
jasminsumaiya <sumaiya@provility.com>
2021-01-28 19:37:34 +0530

    added git ignore file

--- my_ppm/ppm.js
+++ my_ppm/ppm.js
@@ -1,68 +1,101 @@
+'use strict';
 
 const fs = require('fs');
 const fetch = require("node-fetch");
 const path = require('path'); 
 const AnyFile = require('any-file');
+const http = require('https');
+const request = require('superagent');
 
 fs.readFile('dependencies.json', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  let dep = JSON.parse(data);      
-  let dependancies = dep.libs;
+       
+  let dependancies = JSON.parse(data);
   //console.log(dependancies);
 
   fetch("https://packagemanager.vercel.app/registry.json")
         .then(res => res.json())
         .then(data => {
             let registry = data.configs;
+            //console.log("registry");
             //console.log(registry);
 
-            dependancies.forEach((depData) => {
-                let registryItem = registry.find((registryData) => depData.name == registryData.name && depData.version == registryData.version);
-                //console.log(registryItem);
+            let dir = path.join(__dirname, 'ppm_modules/');
+            fs.rmdirSync(dir, {recursive: true});
+            console.log("Folder Deleted!");
 
-                let dir = path.join(__dirname, 'ppm_modules');
+            fs.mkdirSync(dir);
+            console.log('Directory created successfully!'); 
 
-                if (!fs.existsSync(dir)){
-                    fs.mkdirSync(dir);
-                    console.log('Directory created successfully!'); 
-                }
-
-                if(registryItem != undefined){
-                    creatModule(registryItem);
-                }else {
-                    console.log(` Module: ${depData.name} not found`);
-                }
-            });
+            downloadPPMModules(dependancies, registry);
         });
 });
 
+function downloadPPMModules(dependancies, registry){
+    
+    for(var key in dependancies) {
+        let registryItem = registry.find((registryData) => key == registryData.name && dependancies[key] == registryData.version);
+        //console.log(`registryItem ${JSON.stringify(registryItem)}`);
+
+        if(registryItem != undefined){
+            creatModule(registryItem);
+        }else {
+            console.log(` Module: ${key} not found`);
+        }
+    }
+}
    
 function creatModule(registryItem) {
     let fileName = registryItem.name;
-    const af = new AnyFile();
+    let af = new AnyFile();
     let fromFile = registryItem.path;
-    let toFile = path.join(__dirname, 'ppm_modules', fileName);
+    let toFile = path.join(__dirname, 'ppm_modules', `${fileName}.zip`);
 
-        af.from(fromFile).to(toFile, function(err, res) {
-            if (res) {
-            console.log("File copied!");
-            } else {
-            console.log("File not copied!");
-            }
-        });
+    console.log(`fromfile: ${fromFile}`);
+    console.log(`toFile: ${toFile}`);
+    
+    fs.mkdirSync(toFile);
+
+    request.get(fromFile).on('error', (error) => {
+            console.log(error);
+        })
+        .pipe(fs.createWriteStream(toFile))
+        .on('finish', () => {
+            console.log("finish");
+    });  
 }
 
-/*fs.writeFile(modulePath, Buffer.alloc(data, 'utf-8'), function (err) {
-                if (err) throw err;
-                console.log('Saved!');
-            });*/
+/*
+    af.from(fromFile).to(toFile, function(err, res) {
+        if (res) {
+            console.log("File copied!");
+        } else {
+            console.log("File not copied!");
+        }
+    });
 
+    download(fromFile, toFile, function(err) {
+        if (err) {
+            console.log("File not copied!");
+        } else {
+            console.log("File copied!");
+        }
+    });
 
-
-
-
-
+    function download(url, dest, cb) {
+        var file = fs.createWriteStream(dest);
+        http.get(url, (response) => {
+            response.pipe(file);
+                file.on('finish', () => {
+                file.close(cb);  
+                });
+        }).on('error', (err) => { 
+            fs.unlink(dest); 
+            if (cb) cb(err.message);
+        });
+    };
+ */
 
 
