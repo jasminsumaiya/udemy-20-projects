ae25fbb3bc1af496e40fe63a179427c1b6152b08
jasminsumaiya <sumaiya@provility.com>
2021-01-29 16:02:48 +0530

    added npm adm-zip

--- my_ppm/ppm.js
+++ my_ppm/ppm.js
@@ -6,6 +6,25 @@ const path = require('path');
 const AnyFile = require('any-file');
 const http = require('https');
 const request = require('superagent');
+var AdmZip = require('adm-zip');
+
+
+let dir = path.join(__dirname, 'ppm_modules/');
+fs.rmdirSync(dir, {recursive: true});
+console.log("Folder Deleted!");
+
+fs.mkdirSync(dir);
+console.log('Directory created successfully!'); 
+
+/*let registryItem = {
+    "name": "lodash",
+    "version": "1.0.0",
+    "path": "https://ppm-registry.s3.ap-south-1.amazonaws.com/lodash/1.0.0.zip"
+}
+creatModule(registryItem);
+
+return;*/
+
 
 fs.readFile('dependencies.json', 'utf8', function (err,data) {
   if (err) {
@@ -51,23 +70,13 @@ function creatModule(registryItem) {
     let fileName = registryItem.name;
     let af = new AnyFile();
     let fromFile = registryItem.path;
-    let toFile = path.join(__dirname, 'ppm_modules', `${fileName}.zip`);
+    let toFile = path.join(__dirname, 'ppm_modules', `${fileName}.zip`); 
 
     console.log(`fromfile: ${fromFile}`);
     console.log(`toFile: ${toFile}`);
     
     fs.mkdirSync(toFile);
-
-    request.get(fromFile).on('error', (error) => {
-            console.log(error);
-        })
-        .pipe(fs.createWriteStream(toFile))
-        .on('finish', () => {
-            console.log("finish");
-    });  
-}
-
-/*
+    
     af.from(fromFile).to(toFile, function(err, res) {
         if (res) {
             console.log("File copied!");
@@ -75,6 +84,39 @@ function creatModule(registryItem) {
             console.log("File not copied!");
         }
     });
+}
+
+
+
+/*
+
+    request.get({url: fromFile, encoding: null}, (err, res, body) => {
+        if(err) {
+            return console.error(`Error occurred while download a module - ${fromFile}`)
+        }
+        var zip = new AdmZip(body);
+        zip.extractAllTo(toFile, true);
+    });
+
+
+    request.get(fromFile).on('error', (error) => {
+            console.log(error);
+        })
+        .pipe(fs.createWriteStream(toFile))
+        .on('finish', () => {
+            console.log("finish");
+    });
+
+
+    af.from(fromFile).to(toFile, function(err, res) {
+        if (res) {
+            console.log("File copied!");
+        } else {
+            console.log("File not copied!");
+        }
+    });
+
+    ////////
 
     download(fromFile, toFile, function(err) {
         if (err) {
