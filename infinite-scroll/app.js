b970f11b695cfcb76463e4c8938ff14c47ef6243
jasminsumaiya <sumaiya@provility.com>
2021-02-11 18:39:04 +0530

    completed infinite scroll project

--- infinite-scroll/app.js
+++ infinite-scroll/app.js
@@ -1,8 +1,16 @@
 window.addEventListener("load", init);
 let limit = 3;
-let page = 1;
+let page = 2;
 
 function init() {
+  let findBox = document.getElementById("find-box");
+  findBox.addEventListener("keyup", (e) => {
+    if (e.keyCode == 13) {
+      event.preventDefault();
+      onFilterPost();
+    }
+  });
+
   showPost();
 
   window.addEventListener("scroll", () => {
@@ -10,16 +18,7 @@ function init() {
 
     if (scrollTop + clientHeight > scrollHeight - 5) {
       showLoading();
-
-      console.log(
-        "scrolling down",
-        "scrollTop:",
-        scrollTop,
-        "scrollHeight",
-        scrollHeight,
-        "clientHeight:",
-        clientHeight
-      );
+      //console.log("scrollTop:",scrollTop,"scrollHeight",scrollHeight,"clientHeight:",clientHeight);
     }
   });
 }
@@ -30,10 +29,10 @@ function showLoading() {
   setTimeout(() => {
     loader.classList.remove("show");
     setTimeout(() => {
-      page = page + 1;
+      limit = limit + 3;
       showPost();
-      console.log("scrolled");
-    }, 1000);
+      //console.log("scrolled");
+    }, 200);
   }, 3000);
 }
 
@@ -48,8 +47,40 @@ async function getPost() {
 async function showPost() {
   let container = document.getElementById("infinite-con");
   let posts = await getPost();
-  console.log(posts);
+  //console.log(posts);
   container.innerHTML = posts
+    .map((post, index) => {
+      return `<div class="sl-no" id=""sl-no">${index + 1}</div>
+                                <div class="content">
+                                    <strong>
+                                        ${post.title}
+                                    </strong>
+                                    <p>
+                                        ${post.body}
+                                    </p>
+                                </div>`;
+    })
+    .join(" ");
+}
+
+async function allPost() {
+  let res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  let data = await res.json();
+  return data;
+}
+
+async function onFilterPost() {
+  let findBox = document.getElementById("find-box").value.toLowerCase();
+  let container = document.getElementById("infinite-con");
+  let allPosts = await allPost();
+  console.log("filtered");
+
+  let matchedPost = allPosts.filter((post) => {
+    return post.title.toLowerCase().includes(findBox);
+  });
+  console.log(matchedPost);
+
+  container.innerHTML = matchedPost
     .map((post, index) => {
       return `<div class="sl-no" id=""sl-no">${index + 1}</div>
                                 <div class="content">
